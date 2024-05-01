0441a6fac295bd3a7ac32487a76a8dd4ce148d94
marina <m.zagaynova@i-sys.ru>
2024-05-01 14:09:44 +0400

    edit

--- pages/lk_page.py
+++ pages/lk_page.py
@@ -10,15 +10,19 @@ class LkPage(BasePage):
     def button_login_click(self):
         self.click_to_element(LkPageLocators.entrance_button)
 
-    @allure.step("Проверка нахождения на странице авторизации")
-    def check_login(self):
-        assert self.get_text_from_element(LkPageLocators.text_entrance) == 'Вход'
+    @allure.step("Ожидание загрузки странице авторизации")
+    def wait_login(self):
+        self.wait(LkPageLocators.text_entrance)
 
     @allure.step("Авторизация пользователя")
     def authorization(self):
         self.send_text_to_element(LkPageLocators.authorization_email, data.email)
         self.send_text_to_element(LkPageLocators.authorization_password, data.password)
         self.click_to_element(LkPageLocators.button_authorization)
+
+    @allure.step("Проверка нахождения в ЛК")
+    def check_lk_page(self):
+        assert self.get_text_from_element(LkPageLocators.history_order) == "История заказов"
 
     @allure.step("Переход на страницу История заказов")
     def button_order_click(self):
@@ -37,4 +41,3 @@ class LkPage(BasePage):
     def check_exit(self):
         self.wait(LkPageLocators.text_entrance)
         assert self.get_redirected_url() == data.DataUrl.URL + data.DataUrl.LOGIN
-
--- tests/test_lk_page.py
+++ tests/test_lk_page.py
@@ -4,15 +4,29 @@ import allure
 @allure.suite('Личный кабинет')
 class TestLkPage:
 
-    @allure.title('Тест перехода в Личный кабинет, перехода в раздел История заказов и Выход')
-    def test_check_lk_and_history_orders_and_logout(self, lk_page):
+    @allure.title('Тест перехода в Личный кабинет')
+    def test_check_go_to_lk(self, lk_page):
         lk_page.button_login_click()
-        lk_page.check_login()
+        lk_page.wait_login()
+        lk_page.authorization()
+        lk_page.button_login_click()
+        lk_page.check_lk_page()
+        lk_page.button_exit_click()
+
+    @allure.title('Тест перехода в раздел История заказов в личном кабинете')
+    def test_check_lk_history_orders(self, lk_page):
+        lk_page.button_login_click()
         lk_page.authorization()
         lk_page.button_order_click()
         lk_page.check_history_orders()
         lk_page.button_exit_click()
-        lk_page.check_exit()
 
+    @allure.title('Тест выхода из ЛК')
+    def test_check_lk_logout(self, lk_page):
+        lk_page.button_login_click()
+        lk_page.authorization()
+        lk_page.button_login_click()
+        lk_page.button_exit_click()
+        lk_page.check_exit()
 
 
